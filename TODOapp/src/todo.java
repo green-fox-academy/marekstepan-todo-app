791cec21ed03c84891a45cc2d25f99239ad4493f
Marek Stepan <64471500+marekstepan@users.noreply.github.com>
2020-07-11 22:35:48 +0200

    todo done

--- TODOapp/src/todo.java
+++ TODOapp/src/todo.java
@@ -17,18 +17,54 @@ public class todo {
                     listTasks();
                     break;
                 case "-a":
-                    System.out.println("*"+ args[1] + "* just added");
-                    String newLine = System.lineSeparator() + args[1];
-                    addNewTask(newLine);
-                    break;
+                    if (args.length == 1) {
+                        System.out.println("Unable to check: no task provided.");
+                        break;
+                    } else {
+                        System.out.println("*" + args[1] + "* just added.");
+                        String newLine = System.lineSeparator() + args[1];
+                        addNewTask(newLine);
+                        break;
+                    }
                 case "-c":
+                    if (args.length == 1) {
+                        System.out.println("Unable to check: no index provided.");
+                        break;
+                    }
+                    try {
+                        Integer.parseInt(args[1]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Unable to check: index is not a number.");
+                        break;
+                    }
+                    if (Integer.parseInt(args[1]) > countTasks()) {
+                        System.out.println("Unable to check: index is out of bound.");
+                        break;
+                    }
                     int numArg2c = Integer.parseInt(args[1]);
-                    checkTask(numArg2c-1);
+                    checkTask(numArg2c - 1);
                     break;
                 case "-r":
-                    int numArg2r = Integer.parseInt(args[1]);
-                    removeTask(numArg2r-1);
-
+                    if (args.length == 1) {
+                        System.out.println("Unable to remove: no index provided.");
+                        break;
+                    }
+                    try {
+                        Integer.parseInt(args[1]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Unable to remove: index is not a number.");
+                        break;
+                    }
+                    if (Integer.parseInt(args[1]) > countTasks()) {
+                        System.out.println("Unable to remove: index is out of bound.");
+                        break;
+                    } else {
+                        int numArg2r = Integer.parseInt(args[1]);
+                        removeTask(numArg2r - 1);
+                    }
+                default:
+                    System.out.println("Sorry, unsupported argument. Try to be more intelligent.");
+                    instructions();
             }
         }
     }
@@ -47,14 +83,19 @@ public class todo {
 
     public static void listTasks() {
         try {
-            Path path = Paths.get("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\tasks.txt");
+            Path path = Paths
+                .get("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\tasks.txt");
             List<String> lines = Files.readAllLines(path);
             if (lines.isEmpty()) {
                 System.out.println("Hooray! No toodoos for tooday.");
             } else {
                 for (int i = 0; i < lines.size(); i++) {
                     int count = i + 1;
-                    System.out.println(count + " - " + lines.get(i));
+                    if (lines.get(i).charAt(0) == '+') {
+                        System.out.println(count + " - [X] " + lines.get(i).substring(1));
+                    } else {
+                        System.out.println(count + " - [ ] " + lines.get(i));
+                    }
                 }
             }
         } catch (IOException e) {
@@ -65,7 +106,8 @@ public class todo {
     public static void addNewTask(String newLine) {
 
         try {
-            Path path = Paths.get("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\tasks.txt");
+            Path path = Paths
+                .get("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\tasks.txt");
             Files.write(path, newLine.getBytes(), StandardOpenOption.APPEND);
 
         } catch (IOException e) {
@@ -74,17 +116,10 @@ public class todo {
     }
 
     public static void checkTask(int argCount) {
-        try {
-            String checkedLine = Files.readAllLines(Paths.get("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\tasks.txt")).get(argCount);
-            System.out.println(checkedLine);
-        } catch (IOException e) {
-            System.out.println("Houston, we have a problem.");
-        }
-    }
-
-    public static void removeTask(int argCount) {
-        File oldFile = new File("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\tasks.txt");
-        File newFile = new File ("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\temp.txt");
+        File oldFile =
+            new File("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\tasks.txt");
+        File newFile =
+            new File("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\temp.txt");
         try {
             Path path = Paths.get("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\tasks.txt");
             newFile.createNewFile();
@@ -93,9 +128,12 @@ public class todo {
             for (int i = 0; i < lines.size(); i++) {
                 if (i != argCount) {
                     temp.add(lines.get(i));
+                } else {
+                    temp.add('+'+lines.get(i));
                 }
             }
-            Files.write(Paths.get("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\temp.txt"),
+            Files.write(Paths
+                    .get("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\temp.txt"),
                 temp, StandardOpenOption.APPEND);
             oldFile.delete();
             newFile.renameTo(oldFile);
@@ -103,6 +141,46 @@ public class todo {
         } catch (IOException e) {
             System.out.println("Houston, we have a problem.");
         }
+    }
 
+    public static void removeTask(int argCount) {
+        File oldFile =
+            new File("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\tasks.txt");
+        File newFile =
+            new File("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\temp.txt");
+        try {
+            Path path = Paths
+                .get("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\tasks.txt");
+            newFile.createNewFile();
+            List<String> lines = Files.readAllLines(path);
+            List<String> temp = new ArrayList<>();
+            for (int i = 0; i < lines.size(); i++) {
+                if (i != argCount) {
+                    temp.add(lines.get(i));
+                }
+            }
+            Files.write(Paths
+                    .get("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\temp.txt"),
+                temp, StandardOpenOption.APPEND);
+            oldFile.delete();
+            newFile.renameTo(oldFile);
+
+        } catch (IOException e) {
+            System.out.println("Houston, we have a problem.");
+        }
+    }
+
+    public static int countTasks() {
+        int count = 0;
+        try {
+            Path path = Paths
+                .get("C:\\Users\\Admin\\greenfox\\marekstepan-todo-app\\TODOapp\\src\\tasks.txt");
+            List<String> lines = Files.readAllLines(path);
+            count = (lines.size() + 1);
+        } catch (IOException e) {
+            System.out.println("Houston, we have a problem.");
+        }
+        return(count);
     }
 }
+
